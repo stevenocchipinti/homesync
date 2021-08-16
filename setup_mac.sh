cacf6ed055ca005ea0b2210c4df9646381648389
Steve Occhipinti <dev@stevenocchipinti.com>
2021-08-16 23:47:22 +1000

    Use git-delta instead of diff-so-fancy
    This also updates the setup_mac script to install it by default too.

--- setup_mac.sh
+++ setup_mac.sh
@@ -23,17 +23,12 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fish --command="fisher install jethrokuan/z"
 
 # Generic stuff
-brew install cask wget nmap git exa bat keepingyouawake visual-studio-code
-brew cask install iterm2 slack docker
+brew install git exa bat fd git-delta ripgrep vifm gnu-sed
+brew install iterm2 visual-studio-code docker keepingyouawake
 
 # Vim
 brew install neovim
 git clone https://github.com/stevenocchipinti/nvim ~/.config/nvim
-
-# Ruby
-#brew install chruby ruby-install
-#ruby-install ruby --no-reinstall
-#gem install bundle nokogiri
 
 # Javascript
 curl -fsSL https://fnm.vercel.app/install | bash
